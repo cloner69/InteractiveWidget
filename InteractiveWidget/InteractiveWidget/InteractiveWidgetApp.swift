23205a9336df2cbc75e5da0e74040abf6d3e2f88
AdrianSA <50012235+cloner69@users.noreply.github.com>
2025-05-17 03:08:35 +0700

    interactive widget

---
+++ InteractiveWidget/InteractiveWidget/InteractiveWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveWidgetApp.swift
+//  InteractiveWidget
+//
+//  Created by Adrian Suryo Abiyoga on 20/02/25.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InteractiveWidget/TaskWidget/TaskWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TaskWidgetBundle.swift
+//  TaskWidget
+//
+//  Created by Adrian Suryo Abiyoga on 20/02/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TaskWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TaskWidget()
+    }
+}
